cd82499cc6d532f26c10cbe9321c9be7c473c100
Giles <me@glavelle.co.uk>
2011-07-27 14:05:05 +0100

    Progress bar implemented. Lots of work still needed.

--- DDDD/src/uk/co/glavelle/dddd/D4.java
+++ DDDD/src/uk/co/glavelle/dddd/D4.java
@@ -3,45 +3,53 @@ package uk.co.glavelle.dddd;
 import android.app.*;
 import android.os.*;
 import android.content.*;
+import android.widget.*;
+
+import android.content.Intent;
 
 import java.net.*;
 import java.util.*;
-import android.content.Intent;
+import java.io.*;
+
 import org.w3c.dom.*;
 import org.xml.sax.InputSource;
-import java.io.*;
-import javax.xml.parsers.*;
-import org.apache.http.client.*; import org.apache.http.impl.client.*; import org.apache.http.client.methods.*;
 
+import javax.xml.parsers.*;
+
+import org.apache.http.client.*;
+import org.apache.http.impl.client.*;
+import org.apache.http.client.methods.*;
 
 public class D4 extends Activity
 {
+    ProgressDialog progressDialog;
+    ProgressThread progressThread;
+    Handler mHandler;
+    int progress = 0;
+    String url;
+    static final int PROGRESS_DIALOG = 0;
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
 
         if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
             // We can read and write the media
-            Intent intent = getIntent();
-            String url;
             
             try{
-                url = (String) intent.getExtras().get(Intent.EXTRA_TEXT);
-                url += "#view:list";
+                url = (String) getIntent().getExtras().get(Intent.EXTRA_TEXT) + "#view:list";
             }
             catch(Exception e){
-                url = "https://www.dropbox.com/s/8owsfcia59ko76i#view:list"; //Used for testing if it's not opened from the dropox action menu
+                url = "https://www.dropbox.com/s/8owsfcia59ko76i#view:list";
+                //Used for testing if it's not opened from the dropox action menu
                 //Obviously what needs to happen here is a message suggesting that the user use the dropbox app, and a textbox in case they've got the url copied.
             }
-            debug(url);
             
-            String page = getData(url);
-            List filesAndFolders = parsePage(page);
-            String dlDirPath = Environment.getExternalStorageDirectory() + "/download/" + getFolderName(page) + "/";
-            getFilesFromFolder(filesAndFolders, dlDirPath);
-            debug("Done!");
+            showDialog(PROGRESS_DIALOG); //Do the work in a separate thread so we can update the UI simultaneously.
+            
         }
         else {
             //Can't write files, so show a message and quit.
@@ -49,42 +57,78 @@ public class D4 extends Activity
         }
     }
     
-    public void getFilesFromFolder(List filesAndFolders, String savePath){
-        // create a File object for the parent directory
-        File downloadsDirectory = new File(savePath);
-        //create the folder if needed.
-        downloadsDirectory.mkdir();
-
-        for (int i = 0; i < filesAndFolders.size(); i++){
-           Object links = filesAndFolders.get(i);
-           List linksArray = (ArrayList) links;
-           if(i == 0){
-               for (int j = 0; j < linksArray.size(); j+=2){
-                   //We've got an array of file urls so download each one to a directory with the folder name
-                   String fileURL = linksArray.get(j).toString();
-                   String fileName = linksArray.get(j + 1).toString();
-                   downloadFile(fileURL, savePath, fileName);
-               }
-           }
-           else if(i == 1){
-               //we've got an array of folders so recurse down the levels, extracting subfolders and files until we've downloaded everything.
-               for (int j = 0; j < linksArray.size(); j+=2){
-                   String folderURL = linksArray.get(j).toString();
-                   String folderName = linksArray.get(j + 1).toString();
-                   
-                   String page = getData(folderURL);
-                   List newFilesAndFolders = parsePage(page);
-                   String dlDirPath = savePath + folderName + "/";
-
-                   getFilesFromFolder(newFilesAndFolders, dlDirPath);
-                    
-               }
-           }
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch(id) {
+        case PROGRESS_DIALOG:
+            progressDialog = new ProgressDialog(D4.this);
+            progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            progressDialog.setMessage("Loading...");
+            return progressDialog;
+        default:
+            return null;
         }
     }
     
-    public void downloadFile(String url, String savePath, String fileName){
+    // Define the Handler that receives messages from the thread and update the progress
+    final Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            int total = msg.arg1;
+            progressDialog.setProgress(total);
+            if (total >= 100){
+                dismissDialog(PROGRESS_DIALOG);
+                progressThread.setState(ProgressThread.STATE_DONE);
+            }
+        }
+    };
+
+    @Override
+    protected void onPrepareDialog(int id, Dialog dialog) {
+            switch(id) {
+            case PROGRESS_DIALOG:
+                progressDialog.setProgress(0);
+                progressThread = new ProgressThread(handler);
+                progressThread.start();
+        }
+    }
+
+    /** Nested class that performs progress calculations (counting) */
+    private class ProgressThread extends Thread {
         
+        final static int STATE_DONE = 0;
+        final static int STATE_RUNNING = 1;
+        int mState;
+        int total;
+       
+        ProgressThread(Handler h) {
+            mHandler = h;
+        }
+        
+        @Override
+        public void run() {
+            mState = STATE_RUNNING;   
+            total = 0;
+            stuff();
+        }
+        
+        /* sets the current state for the thread,
+         * used to stop the thread */
+        public void setState(int state) {
+            mState = state;
+        }
+    }
+    
+    public void stuff(){
+        String page = getData(url);
+        List filesAndFolders = parsePage(page);
+        String dlDirPath = Environment.getExternalStorageDirectory() + "/download/" + getFolderName(page) + "/";
+        getFilesFromFolder(filesAndFolders, dlDirPath);
+        debug("Done!");
+    }
+            
+    public void downloadFile(String url, String savePath, String fileName){
+            
         try {
             URL theURL = new URL(url);
             InputStream input = theURL.openStream();
@@ -105,6 +149,44 @@ public class D4 extends Activity
         } 
         catch(Exception e){
             debug(e.toString());
+        }
+    }
+    
+    public void getFilesFromFolder(List filesAndFolders, String savePath){
+        // create a File object for the parent directory
+        File downloadsDirectory = new File(savePath);
+        //create the folder if needed.
+        downloadsDirectory.mkdir();
+
+        for (int i = 0; i < filesAndFolders.size(); i++){
+           Object links = filesAndFolders.get(i);
+           List linksArray = (ArrayList) links;
+           if(i == 0){
+               for (int j = 0; j < linksArray.size(); j+=2){
+                   //We've got an array of file urls so download each one to a directory with the folder name
+                   String fileURL = linksArray.get(j).toString();
+                   String fileName = linksArray.get(j + 1).toString();
+                   downloadFile(fileURL, savePath, fileName);
+                   progress++;
+                   Message msg = mHandler.obtainMessage();
+                   msg.arg1 = progress;
+                   mHandler.sendMessage(msg);
+               }
+           }
+           else if(i == 1){
+               //we've got an array of folders so recurse down the levels, extracting subfolders and files until we've downloaded everything.
+               for (int j = 0; j < linksArray.size(); j+=2){
+                   String folderURL = linksArray.get(j).toString();
+                   String folderName = linksArray.get(j + 1).toString();
+                   
+                   String page = getData(folderURL);
+                   List newFilesAndFolders = parsePage(page);
+                   String dlDirPath = savePath + folderName + "/";
+
+                   getFilesFromFolder(newFilesAndFolders, dlDirPath);
+                    
+               }
+           }
         }
     }
     
